4202cb174805bd039507ed86e22b610705c7da37
Link <bal2457@utexas.edu>
2021-10-14 23:25:37 -0500

    Added starter database files
    Basic database with create note function added

---
+++ asp.net web api/Notes.WebApo/Notes.Core/NotesServices.cs
@@ -0,0 +1,22 @@
+﻿
+using Notes.DB;
+
+namespace Notes.Core
+{
+    public class NotesServices
+    {
+        private AppDbContext _context;
+        public NotesServices(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Note CreateNote(Note note)
+        {
+            _context.Add(note);
+            _context.SaveChanges();
+
+            return note;
+        }
+    }
+}
---
+++ asp.net web api/Notes.WebApo/Notes.WebApo/Controllers/NotesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Notes.WebApo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NotesController : ControllerBase
+    {
+        private readonly ILogger<NotesController> _logger;
+
+        public NotesController(ILogger<NotesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult CreateNote()
+        {
+            return Ok("My First Note");
+        }
+    }
+}
